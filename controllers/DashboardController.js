614483ba9101ada2363c80ca734531f5e4c76ba2
Try Indra Hatmojo <tryindra3375@gmail.com>
2025-06-04 15:37:38 +0700

    Fitur Search

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -1,12 +1,24 @@
 const { Stock, Transaction, Portfolio } = require("../models")
 const { formatRupiah } = require("../helpers/helper")
+const { Op } = require("sequelize")
 
 class DashboardController {
 
     static async dashboard(req, res){
         try {
             const { username, balance, role } = req.session
-            const stocks = await Stock.findAll()
+            const { search } = req.query
+            let opt = {}
+
+            if(search){
+                opt.where = {
+                    sector:{
+                        [Op.iLike]: `%${search}%`
+                    }
+                }
+            }
+
+            const stocks = await Stock.findAll(opt)
             res.render("dashboard", { stocks, username, balance, formatRupiah, role })
         } catch (error) {
             res.send(error)
